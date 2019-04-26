b0fbe7aafc5d46ecc1bca2fe830fe7ebf4ece298
baaass2 <jokerbass087@gmail.com>
2019-04-26 12:30:14 -0400

    Se eliminado los temp

--- proyecto1progaav/proyecto1.sh
+++ proyecto1progaav/proyecto1.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 function DescargarBD {
   if [[ -f "bd-pdb.txt" ]]; then
       echo "Base de datos actualmente descargada."
@@ -8,22 +10,22 @@ function DescargarBD {
 }
 
 function DescargarPBD {
-  if [[ -f "$1.txt" ]]; then
+  if [[ -f "$1.pdb" ]]; then
     echo "PDB ya descargado."
 
   else
     echo "Se descargara PDB solicitado con la ID $1."
     wget https://files.rcsb.org/download/$1.pdb
-    mv $1.pdb $1.txt
-    FiltrarPDB $1
   fi
 }
 
 function FiltrarPDB {
   echo "Comenzara la filtracion de archivo PDB."
-  awk '$1 == "ATOM" { print $3" "$4" "$5" "$6 }' $1.txt >> $11.txt
-  awk 'length($1) >= 4 {$1 = substr($1,1,3)" "substr($1,5,6)} { print $0 }' $11.txt >> ATOM$111.txt
-  awk 'length($2) > 3 { $2 = substr($2,2,3)} { print $0 }' ATOM$111.txt >> $1final.txt
+  awk '$1 == "ATOM" { print $3" "$4" "$5" "$6 }' $1.pdb | \
+  awk 'length($1) >= 4 {$1 = substr($1,1,3)" "substr($1,5,6)} { print $0 }' | \
+  awk 'length($2) > 3 { $2 = substr($2,2,3)} { print $0 }' | \
+  awk -v CONVFMT='%.0f ' -f creargrafico.awk | \
+  uniq > "$ID".dot
 }
 
 DescargarBD
@@ -45,23 +47,22 @@ while [[ $SALIR != "1" ]]; do
 
 done
 
-PROTEINA=`grep -i "$ID" bd-pdb.txt | rev | awk -F, '{ print $1 }' | rev`
+PROTEINA=`grep -F "$ID" bd-pdb.txt | rev | awk -F, '{ print $1 }' | rev`
 
 SALIR="0"
 while [[ $SALIR != "1" ]]; do
   if [[ $PROTEINA == '"Protein"' ]]; then
     echo "Existe proteina en la BD."
     DescargarPBD $ID
+    FiltrarPDB $ID
     SALIR="1"
-  elif [[ $PROTEINA != '"Protein"' ]]; then
-    echo "ID Incorrecto."
+  else
+    echo "ID no es una proteina."
     echo "Ingrese nuevamente proteina: "
     read ID
     ID=${ID^^}
-    PROTEINA=`grep -i "$ID" bd-pdb.txt | rev | awk -F, '{ print $1 }' | rev`
+    PROTEINA=`grep -F "$ID" bd-pdb.txt | rev | awk -F, '{ print $1 }' | rev`
   fi
 done
 
-awk -v CONVFMT='%.0f ' -f creargrafico.awk "$ID"final.txt >> "$ID"final.dot
-uniq "$ID"final.dot >> "$ID".dot
 dot -Tsvg -o "$ID".svg "$ID".dot
