f6b11cc8d699647ef7b108ad0b4c4b8673837109
baaass2 <jokerbass087@gmail.com>
2019-05-08 18:28:47 -0400

    Se ha ordenado la salida de archivos

--- proyecto2progaav/proyecto2.sh
+++ proyecto2progaav/proyecto2.sh
@@ -11,7 +11,7 @@ function DescargarBD {
 }
 
 function DescargarPBD {
-    if [[ -f "$1.pdb" ]]; then
+    if [[ -d "$1" ]]; then
         echo "PDB ya descargado."
     else
         echo "Se descargara PDB solicitado con la ID $1."
@@ -21,10 +21,10 @@ function DescargarPBD {
 
 function FiltrarPDB {
     echo "Comenzara la filtracion de archivo PDB."
-    awk '$1 == "HETATM" && $4 != "HOH" { print $4" "$5" "$6" "$7" "$8" "$9 }' $1.pdb >> hetatm_$1.txt
+    awk '$1 == "HETATM" && $4 != "HOH" { print $4" "$5" "$6" "$7" "$8" "$9 }' $1.pdb > hetatm_$1.txt
     awk '$1 == "ATOM" { print $2" "$3" "$4" "$5" "$6" "$7" "$8" "$9 }' $1.pdb | \
     awk 'length($2) >= 4 {$2 = substr($2,1,3)" "substr($2,5,7)} { print $0 }' | \
-    awk 'length($3) > 3 { $3 = substr($3,2,4)} { print $0 }' >> atom_$1.txt
+    awk 'length($3) > 3 { $3 = substr($3,2,4)} { print $0 }' > atom_$1.txt
 }
 
 function LigandoPDB {
@@ -36,12 +36,12 @@ function LigandoPDB {
         ligando=`ls | grep -i "$1lig" | sed -n "$i p"`
         echo $ligando
         LIGA=${ligando:7}
-        awk -v CONVFMT='%.0f ' -f $Ractual/centrogeometrico.awk $ligando >> vector$ligando.txt
+        awk -v CONVFMT='%.0f ' -f $Ractual/centrogeometrico.awk $ligando > vector$ligando.txt
         X=`sed -n '1 p' vector$ligando.txt`
         Y=`sed -n '2 p' vector$ligando.txt`
         Z=`sed -n '3 p' vector$ligando.txt`
-        awk -v Xcg=$X -v Ycg=$Y -v Zcg=$Z -v DIS=$2 -v CONVFMT='%.0f ' -f $Ractual/creargrafico.awk atom_$1.txt >> final$ligando.dot
-        dot -Tpng -o $Ractual/$LIGA.png $Ractual/$1/final$ligando.dot
+        awk -v Xcg=$X -v Ycg=$Y -v Zcg=$Z -v DIS=$2 -v CONVFMT='%.0f ' -f $Ractual/creargrafico.awk atom_$1.txt > final$ligando.dot
+        dot -Tpng -o $Ractual/render_$1/$LIGA.png $Ractual/$1/final$ligando.dot
 
     done
 
@@ -54,12 +54,10 @@ while [[ "$OPC" != "N" ]]; do
     echo "Ingrese ID de Proteina: "
     read ID
     ID=${ID^^}
-    echo "Ingrese distancia: "
-    read DIS
 
     SALIR="0"
     while [[ $SALIR != "1" ]]; do
-        if [[ -d "$ID" ]]; then
+        if [[ -d "render_$ID" ]]; then
             echo "La proteina seleccionada ya tiene construido su grafico."
             echo "Ingrese otra ID de proteina: "
             read ID
@@ -76,8 +74,10 @@ while [[ "$OPC" != "N" ]]; do
     while [[ $SALIR != "1" ]]; do
     	if [[ $PROTEINA == '"Protein"' ]]; then
             echo "Existe proteina en la BD."
+            echo "Ingrese distancia: "
+            read DIS
 		    mkdir "$ID"
-            mkdir render_$ID
+            mkdir "render_$ID"
             cd "$ID"
             DescargarPBD $ID
             FiltrarPDB $ID
@@ -91,7 +91,7 @@ while [[ "$OPC" != "N" ]]; do
             PROTEINA=`grep -w "$ID" $Ractual/bd-pdb.txt | rev | awk -F, '{ print $1 }' | rev`
         fi
     done
-
+    cd $Racutal
     echo "Se ha finalizado la filtracion y construccion del grafico."
     echo "¿Quiere graficar otra proteina (Y/N)"
     read OPC
